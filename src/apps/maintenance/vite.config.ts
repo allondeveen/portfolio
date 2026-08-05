b9a893df3268b51bdf748fc7375d3a1f632119bf
Allon de Veen <allondeveen@gmail.com>
2026-08-06 01:22:00 +0200

    restart maintenance app from cloudflare template to fix deploy

--- src/apps/maintenance/vite.config.ts
+++ src/apps/maintenance/vite.config.ts
@@ -1,16 +1,33 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv, type Plugin } from "vite";
+
+function removeVanillaExtractExternals(): Plugin {
+  return {
+    name: "remove-vanilla-extract-externals",
+
+    configResolved(config) {
+      const ssrEnvironment = config.environments.ssr;
+
+      if (!ssrEnvironment) {
+        return;
+      }
+
+      ssrEnvironment.resolve.external = [];
+    },
+  };
+}
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const port = parseInt(env.PORT) || 5173;
   return {
     plugins: [
-      reactRouter(),
-      cloudflare({ viteEnvironment: { name: "ssr" } }),
       vanillaExtractPlugin(),
+      removeVanillaExtractExternals(),
+      cloudflare({ viteEnvironment: { name: "ssr" } }),
+      reactRouter(),
     ],
     preview: {
       port,
