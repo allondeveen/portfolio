6f3ffe42ee32fa9f8a0bd12d479923ffa3117ed0
Allon de Veen <allondeveen@gmail.com>
2026-08-06 13:35:41 +0200

    restart website app from cloudflare template to anticipate deploy

---
+++ src/apps/website/workers/app.ts
@@ -0,0 +1,13 @@
+import { createRequestHandler } from "react-router";
+
+const requestHandler = createRequestHandler(
+  // eslint-disable-next-line
+  () => import("virtual:react-router/server-build"),
+  import.meta.env.MODE,
+);
+
+export default {
+  async fetch(request) {
+    return requestHandler(request);
+  },
+} satisfies ExportedHandler<Env>;
