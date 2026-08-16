9289c679f2a9df9b5476dbfa98e12a49161697bb
Allon de Veen <allondeveen@gmail.com>
2026-08-17 01:48:04 +0200

    add document kind to main element's class

--- src/features/public-documents/src/website/document.tsx
+++ src/features/public-documents/src/website/document.tsx
@@ -1,10 +1,11 @@
 import { BlocksComponent } from "@allondeveen-portfolio/blocks-property/website";
+import clsx from "clsx";
 
 import type { Document } from "./data";
 
-export function Document({ blocks, meta: { title, description } }: Document) {
+export function Document({ kind, meta: { title, description }, blocks }: Document) {
   return (
-    <main className="document">
+    <main className={clsx("document", kind)}>
       <title>{title}</title>
       <meta name="description" content={description} />
       <section className="document__content">
