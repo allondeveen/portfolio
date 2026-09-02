61681f6fe32a9a884a47c0d85b8357f958cb1557
Allon de Veen <allondeveen@gmail.com>
2026-09-02 19:26:09 +0200

    don't export maintenance content adapter

--- src/features/entities/maintenance/src/trpc-server.ts
+++ src/features/entities/maintenance/src/trpc-server.ts
@@ -1,3 +1,2 @@
-export { mapMaintenanceContent } from "./trpc-server/adapter";
 export { maintenanceProcedure } from "./trpc-server/maintenance";
 export { type MaintenanceContentRouter, maintenanceContentRouter } from "./trpc-server/router";
