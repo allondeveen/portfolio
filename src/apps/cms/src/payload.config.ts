74a4f9bbb3419141ab8a113ac6ea6630e31fc491
Allon de Veen <allondeveen@gmail.com>
2026-09-09 15:16:25 +0200

    speed up dev

--- src/apps/cms/src/payload.config.ts
+++ src/apps/cms/src/payload.config.ts
@@ -188,8 +188,8 @@ export default buildConfig({
   db: postgresAdapter({
     pool: {
       connectionString: cloudflare.env.DATABASE_CONNECTIONSTRING,
-      maxUses: 1,
-      connectionTimeoutMillis: 10_000,
+      maxUses: cloudflare.env.ENVIRONMENT === "development" ? undefined : 1,
+      connectionTimeoutMillis: cloudflare.env.ENVIRONMENT === "development" ? undefined : 10_000,
     },
   }),
   logger: isProduction ? cloudflareLogger : undefined,
