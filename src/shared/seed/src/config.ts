6b2aabb7fd96eb11175366e511663c2b6469d1be
Allon de Veen <allondeveen@gmail.com>
2026-09-14 20:26:23 +0200

    add seeds for not found page

--- src/shared/seed/src/config.ts
+++ src/shared/seed/src/config.ts
@@ -3,6 +3,7 @@ import {
   maintenanceSeeds,
 } from "@allondeveen-portfolio/maintenance-content/seed";
 import { menuSeeds } from "@allondeveen-portfolio/menu/seed";
+import { notFoundIsInitialised, notFoundSeeds } from "@allondeveen-portfolio/not-found/seed";
 import { pageSeeds } from "@allondeveen-portfolio/pages/seed";
 import {
   siteSettingsIsInitialised,
@@ -105,6 +106,8 @@ export function onInit(seedEmail: string = "", seedPass: string = "") {
     await seed("templates", templateSeeds(payload));
 
     await globalSeed("maintenance", maintenanceIsInitialised, maintenanceSeeds(payload));
+
+    await globalSeed("not-found", notFoundIsInitialised, notFoundSeeds(payload));
 
     console.log("Seeding finished");
   };
